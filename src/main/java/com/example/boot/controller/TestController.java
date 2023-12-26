df7bb1586117c6e44d5d31ef8b30fe6f107a83fd
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-12-26 20:08:20 +0900

    ++

--- src/main/java/com/example/boot/controller/TestController.java
+++ src/main/java/com/example/boot/controller/TestController.java
@@ -3,9 +3,27 @@ package com.example.boot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TestController {
+
+    @RequestMapping("/test")
+    public ModelAndView test() throws Exception{
+        ModelAndView mav = new ModelAndView("test");
+        mav.addObject("name", "mbj");
+
+        List<String> testList = new ArrayList<>();
+        testList.add("a");
+        testList.add("b");
+        testList.add("c");
+
+        mav.addObject("list", testList);
+        return mav;
+    };
 
     @RequestMapping("/home")
     public String Home() {
